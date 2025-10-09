edc5255f733625707cd0b0c597b3a782369f7b38
Mahmoud Mahfouz <mahmod202350@gmail.com>
2025-10-09 23:47:55 +0300

    add root add bottom navigate bar

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hangery/core/routing/routs.dart';
 import 'package:hangery/featuer/auth/view/login_view.dart';
 import 'package:hangery/featuer/auth/view/sign_up_view.dart';
+import 'package:hangery/featuer/cart/view/cart_page.dart';
+import 'package:hangery/featuer/cheakout/view/checkou_page.dart';
+import 'package:hangery/featuer/home/view/home_page.dart';
+import 'package:hangery/featuer/order_history.dart/view/order_history.dart';
+import 'package:hangery/root.dart';
 import 'package:hangery/splash_screen.dart';
 
 class AppRouter {
@@ -15,6 +20,17 @@ class AppRouter {
 
       case Routs.signUp:
         return MaterialPageRoute(builder: (_) => SignUpView());
+      case Routs.homePage:
+        return MaterialPageRoute(builder: (_) => HomePage());
+
+      case Routs.cart:
+        return MaterialPageRoute(builder: (_) => CartPage());
+      case Routs.checkout:
+        return MaterialPageRoute(builder: (_) => CheckouPage());
+      case Routs.orderHistory:
+        return MaterialPageRoute(builder: (_) => OrderHistory());
+      case Routs.root:
+        return MaterialPageRoute(builder: (_) => Root());
 
       default:
         return MaterialPageRoute(builder: (_) => Text('Error'));
