cb24844aa66e81d5ac19e8816624182bb82bd163
Mahmoud Mahfouz <mahmod202350@gmail.com>
2025-10-09 16:49:51 +0300

    add login and signup pages

---
+++ lib/featuer/auth/view/login_view.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hangery/core/constant/app_colors.dart';
+import 'package:hangery/core/routing/routs.dart';
+import 'package:hangery/core/sheard/widgets/app_text_feild.dart';
+import 'package:hangery/core/sheard/widgets/coustom_text.dart';
+import 'package:hangery/core/sheard/widgets/custom_text_botton.dart';
+import 'package:hangery/core/utils/naivgators.dart';
+
+// ignore: must_be_immutable
+class LoginView extends StatelessWidget {
+  LoginView({super.key});
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: Column(
+            children: [
+              150.verticalSpace,
+              SvgPicture.asset('assets/svg/Hungry_.svg'),
+              8.verticalSpace,
+              CoustomText(
+                text: 'Welcom Back , descover the fast food',
+                color: Colors.white,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+              ),
+              90.verticalSpace,
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    AppTextFeild(
+                      controller: emailController,
+                      suffixIcon: Icon(Icons.email_outlined),
+
+                      fillColor: Colors.white,
+                      labelText: 'Email',
+                      hintText: 'exampil@.com',
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
+                    22.verticalSpace,
+                    AppTextFeild(
+                      controller: passwordController,
+                      suffixIcon: Icon(Icons.remove_red_eye_outlined),
+
+                      fillColor: Colors.white,
+                      labelText: 'Password',
+                      hintText: '***********',
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    40.verticalSpace,
+                    CustomTextBotton(
+                      backGroundColor: Colors.white,
+                      text: 'Login',
+                      color: AppColors.primaryColor,
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w600,
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          print('Login button pressed');
+                        }
+                      },
+                    ),
+                    22.verticalSpace,
+                    TextButton(
+                      onPressed: () {
+                        context.pushNamed(Routs.signUp);
+                      },
+                      child: CoustomText(
+                        text: 'Create new account',
+                        color: Colors.white,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,9 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hangery/core/constant/app_colors.dart';
+import 'package:hangery/core/routing/routs.dart';
+import 'package:hangery/core/utils/naivgators.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        context.pushNamed(Routs.loginView);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
